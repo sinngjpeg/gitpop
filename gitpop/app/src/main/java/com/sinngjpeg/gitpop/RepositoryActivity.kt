b41d9966f525a46bddb9a2a2d476a39f9f9ed937
Ingrid <ingridsilvatec@outlook.com>
2021-01-08 19:18:06 -0300

    implementa layout, RepositoryActivity

---
+++ gitpop/app/src/main/java/com/sinngjpeg/gitpop/RepositoryActivity.kt
@@ -0,0 +1,15 @@
+package com.sinngjpeg.gitpop
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.sinngjpeg.gitpop.databinding.RepositoryActivityBinding
+
+class RepositoryActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding = RepositoryActivityBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setSupportActionBar(binding.toolbarRepository)
+        supportActionBar?.setDisplayShowHomeEnabled(true)
+    }
+}
