f022184871e994bddc40e6aba11f01b57c16ab62
Ingrid <ingridsilvatec@outlook.com>
2021-01-08 19:35:35 -0300

    Theme.AppCompat.Light.NoActionBar

---
+++ gitpop/app/src/main/java/com/sinngjpeg/gitpop/PullRequestActivity.kt
@@ -0,0 +1,16 @@
+//package com.sinngjpeg.gitpop
+//
+//import android.os.Bundle
+//import androidx.appcompat.app.AppCompatActivity
+//import com.sinngjpeg.gitpop.databinding.RepositoryActivityBinding
+//
+//class PullRequestActivity : AppCompatActivity() {
+//    val binding = RepositoryActivityBinding.inflate(layoutInflater)
+//
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        setContentView(binding.root)
+//        setSupportActionBar(binding.toolbarRepository)
+//        supportActionBar?.setDisplayShowHomeEnabled(true)
+//    }
+//}
--- gitpop/app/src/main/java/com/sinngjpeg/gitpop/RepositoryActivity.kt
+++ gitpop/app/src/main/java/com/sinngjpeg/gitpop/RepositoryActivity.kt
@@ -5,11 +5,13 @@ import androidx.appcompat.app.AppCompatActivity
 import com.sinngjpeg.gitpop.databinding.RepositoryActivityBinding
 
 class RepositoryActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = RepositoryActivityBinding.inflate(layoutInflater)
         setContentView(binding.root)
         setSupportActionBar(binding.toolbarRepository)
-        supportActionBar?.setDisplayShowHomeEnabled(true)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
     }
 }
